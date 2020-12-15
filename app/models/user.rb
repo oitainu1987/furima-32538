df17a82f5047c181b00289feab63f0e56676fdd1
oitainu1987 <74770684+oitainu1987@users.noreply.github.com>
2020-12-15 15:44:34 +0900

    Userモデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,21 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
-  
-  NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
+
+  NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/.freeze
   validates_format_of :first_name, with: NAME_REGEX, presence: true
   validates_format_of :family_name, with: NAME_REGEX, presence: true
 
-  KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
   validates_format_of :first_name_kana, with: KANA_REGEX, presence: true
   validates_format_of :family_name_kana, with: KANA_REGEX, presence: true
 
   validates :birth_day, presence: true
-  
 end
