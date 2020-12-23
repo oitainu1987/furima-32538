ccf54d6345da0de142a72dd2d5d584b0965ddf2f
oitainu1987 <74770684+oitainu1987@users.noreply.github.com>
2020-12-23 10:24:50 +0900

    クレジットカード決済機能実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -5,9 +5,10 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def create
+    
     @user_purchase = UserPurchase.new(user_params)
-    binding.pry
       if @user_purchase.valid?
+         pay_item
          @user_purchase.save
          redirect_to root_path
       else
@@ -18,11 +19,20 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def user_params
-    params.require(:user_purchase).permit(:hoge, :item_id, :user_id, :postal_code, :area_id, :city, :house_number, :building, :tel, :purchase_record_id).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:user_purchase).permit(:hoge, :item_id, :user_id, :postal_code, :area_id, :city, :house_number, :building, :tel, :purchase_record_id).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] #PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  #商品の値段
+      card: user_params[:token], #カードトークン
+      currency: 'jpy' #通過の種類（日本円）
+    )
   end
 
 end
--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -1,13 +1,14 @@
 class UserPurchase
   
   include ActiveModel::Model
-  attr_accessor :hoge, :item_id, :user_id, :postal_code, :area_id, :city, :house_number, :building, :tel, :purchase_record_idq
+  attr_accessor :hoge, :item_id, :user_id, :postal_code, :area_id, :city, :house_number, :building, :tel, :purchase_record_id, :token
 
   with_options presence: true do
     validates :postal_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city,         format: {with:/\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "この項目は必須です"}
     validates :house_number
     validates :tel,          format: {with: /\A\d{11}\z/, message: "半角数字ハイフン無しです"}
+    validates :token
   end
 
   def save
